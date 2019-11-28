25c513975b697608c9cdb11eedf18af1f017c1e7
wardahmad <whitelight43@hotmail.com>
2019-11-28 16:06:12 +0300

    finishedhw-w02d04-dom

--- moreDomManipulation_HW/min.js
+++ moreDomManipulation_HW/min.js
@@ -26,30 +26,36 @@ var books = [
     }
   ];
 
+function myfun(){
+  var list = document.querySelectorAll("li");
+  document.body.style.fontFamily = "Arial, sans-serif";
+  var listArr = ["Nickname: ward","Favorite Animal: Canary","Hometown: Makkah"];
+  var node = document.createElement("H1");
+  var textnode = document.createTextNode("My Book List");
+  node.appendChild(textnode);
+  document.querySelector(".favoriteBooks").appendChild(node);
 
-document.body.style.fontFamily = "Arial, sans-serif";
 
-document.getElementById("nickname").innerHTML = "Ward";
-document.getElementById("favorite").innerHTML = "Canary";
-document.getElementById("hometown").innerHTML = "Makkah";
+  for (var i=0; i<list.length; i++){
+    for (var j=0; j<listArr.length; j++){
+      list[i].style.color = "rebeccapurple";
+      list[i].innerHTML=listArr[i];
+    } 
+    list[i].classList.add("listitem"); 
+  }
 
+  for (var m=0; m<books.length; m++){
+    var node = document.createElement("P");
+    var pNode = document.createTextNode(books[m].title + ", by " + books[m].author);
+    node.appendChild(pNode);
+    document.querySelector(".favoriteBooks").appendChild(node);
+  }
 
-
-document.querySelector("li").classList.add("listitem");
-
-document.querySelector("li").style.color = "rebeccapurple";
-
-var img = document.createElement("IMG");
-img.setAttribute("src","https://images2.minutemediacdn.com/image/upload/c_crop,h_1190,w_2121,x_0,y_134/f_auto,q_auto,w_1100/v1559847832/shape/mentalfloss/24107-istock-1040334020.jpg");
-img.setAttribute("width", "304");
-img.setAttribute("height", "228");
-document.body.appendChild(img);
-
-var node = document.createElement("H1");
-var textnode = document.createTextNode("My Book List");
-node.appendChild(textnode);
-document.querySelector("div.favoriteBooks").appendChild(node);
-
-for (const i in books){
-    books.title[i] + ", by " + books.author[i]
+  var img = document.createElement("IMG");
+  img.setAttribute("src","https://images2.minutemediacdn.com/image/upload/c_crop,h_1190,w_2121,x_0,y_134/f_auto,q_auto,w_1100/v1559847832/shape/mentalfloss/24107-istock-1040334020.jpg");
+  img.setAttribute("width", "500");
+  img.setAttribute("height", "300");
+  var picNode = document.querySelector(".profilePicture");
+  picNode.appendChild(img);
+   
 }
--- pictureGenerator_HW/min.js
+++ pictureGenerator_HW/min.js
@@ -1,11 +1,13 @@
 
 function myImg(width,height){
-    if (width || height !== NaN){
+    if (width || height !== null){
         var wid = document.querySelector(".width").value;
         var hei = document.querySelector(".height").value;
 
+        console.log(wid);
+
         var img = document.getElementById("image1");
-        var result = img.setAttribute("src","https://source.unsplash.com/collection/190727/"+ wid*hei);
+        var result = img.setAttribute("src","https://source.unsplash.com/collection/190727/"+ wid+"x"+hei);
     }else {
         var img = document.getElementById("image1");
         var result = img.setAttribute("src","https://source.unsplash.com/collection/190727/1600x900");
